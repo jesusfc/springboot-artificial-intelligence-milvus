0e06f99e9f98a197e3372b3489f92e5620b56286
Jesusfc <jfcaraballo@gmail.com>
2024-10-04 09:36:37 +0200

    clean code

--- src/main/java/com/jesusfc/springboot_artificial_intelligence_milvus/bootstrap/LoadVectorStore.java
+++ src/main/java/com/jesusfc/springboot_artificial_intelligence_milvus/bootstrap/LoadVectorStore.java
@@ -2,13 +2,22 @@ package com.jesusfc.springboot_artificial_intelligence_milvus.bootstrap;
 
 import com.jesusfc.springboot_artificial_intelligence_milvus.config.MilvusConfig;
 import com.jesusfc.springboot_artificial_intelligence_milvus.config.VectorStoreProperties;
-import io.milvus.v2.client.MilvusClientV2;
-import io.milvus.v2.service.collection.request.HasCollectionReq;
-import io.milvus.v2.service.collection.response.ListCollectionsResp;
-import io.milvus.v2.service.database.response.ListDatabasesResp;
+import io.milvus.client.MilvusServiceClient;
+import io.milvus.grpc.ListDatabasesResponse;
+import io.milvus.param.R;
+import io.milvus.param.collection.HasCollectionParam;
+import io.milvus.param.highlevel.collection.CreateSimpleCollectionParam;
+import io.milvus.param.highlevel.collection.ListCollectionsParam;
+import io.milvus.param.highlevel.collection.response.ListCollectionsResponse;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * Author Jesús Fdez. Caraballo
@@ -37,28 +46,41 @@ public class LoadVectorStore implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        MilvusClientV2 milvusConnection = milvusConfig.createMilvusConnection();
+        MilvusServiceClient milvusConnection = milvusConfig.createMilvusConnection();
 
-        ListDatabasesResp listDatabasesResponse = milvusConnection.listDatabases();
-        System.out.println(listDatabasesResponse.getDatabaseNames());
+        R<ListDatabasesResponse> r = milvusConnection.listDatabases();
+        System.out.println(r.getData());
 
-        HasCollectionReq hasCollectionReq = HasCollectionReq.builder().
-                collectionName("vector_store").build();
+        R<ListCollectionsResponse> lc = milvusConnection.listCollections(ListCollectionsParam.newBuilder().build());
+        System.out.println(lc.getData());
 
-        Boolean b = milvusConnection.hasCollection(hasCollectionReq);
-        System.out.println(b);
+        HasCollectionParam hasCollectionReq = HasCollectionParam.newBuilder()
+                .withDatabaseName("default")
+                .withCollectionName("vector_store")
+                .build();
 
-        ListCollectionsResp listCollectionsResp = milvusConnection.listCollections();
-        System.out.println(listCollectionsResp);
+        R<Boolean> b = milvusConnection.hasCollection(hasCollectionReq);
+        System.out.println(b.getData());
 
+        if (!b.getData()) {
+            // 2. Create a collection in quick setup mode
+            CreateSimpleCollectionParam quickSetupReq = CreateSimpleCollectionParam.newBuilder()
+                    .withCollectionName("vector_store")
+                    .withDimension(5)
+                    .build();
 
+            milvusConnection.createCollection(quickSetupReq);
+        }
 
-        /*
-        if (vectorStore.similaritySearch("Sportsman").isEmpty()){
+        R<ListCollectionsResponse> listCollectionsResp = milvusConnection.listCollections(ListCollectionsParam.newBuilder().build());
+        System.out.println(listCollectionsResp.getData());
+
+       // if (vectorStore.similaritySearch("Sportsman").isEmpty()){
             System.out.println("Loading documents into vector store");
 
             vectorStoreProperties.getDocumentsToLoad().forEach(document -> {
                 System.out.println("Loading document: " + document.getFilename());
+
 
                 TikaDocumentReader documentReader = new TikaDocumentReader(document);
                 List<Document> documents = documentReader.get();
@@ -68,13 +90,15 @@ public class LoadVectorStore implements CommandLineRunner {
                 List<Document> splitDocuments = textSplitter.apply(documents);
 
                 vectorStore.add(splitDocuments);
-            });
-        }
+
+
+           });
+
+        //}
 
         System.out.println("Vector store loaded");
-        
-        */
+
     }
-    
-         
+
+
 }
